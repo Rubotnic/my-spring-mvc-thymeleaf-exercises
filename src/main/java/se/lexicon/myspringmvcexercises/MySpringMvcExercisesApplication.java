adf6f8c9b2495bf78b0a583e83e1010b94c745f3
Rubotnic <info@rubotnic.com>
2022-10-03 21:57:48 +0200

    My Spring MVC Thymeleaf exercises.

---
+++ src/main/java/se/lexicon/myspringmvcexercises/MySpringMvcExercisesApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.myspringmvcexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringMvcExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringMvcExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/myspringmvcexercises/MySpringMvcExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.myspringmvcexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringMvcExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
